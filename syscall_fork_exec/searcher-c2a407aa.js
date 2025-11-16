dac27fa4e4862a34356181653b4d12e4daa04032
giob22 <giovanni.fragola.nl@gmail.com>
2025-11-16 21:54:04 +0100

    aggiornamento automatico

--- syscall_fork_exec/searcher-c2a407aa.js
+++ syscall_fork_exec/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-1b3333c8.js',
+                path_to_root + 'searchindex-1178c0a7.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
