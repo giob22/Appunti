82be5155c6377513f959803245a8264d133c7031
giob22 <giovanni.fragola.nl@gmail.com>
2025-11-18 00:13:00 +0100

    aggiornamento automatico

--- IPC_appunti/searcher-c2a407aa.js
+++ IPC_appunti/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-ff2786ff.js',
+                path_to_root + 'searchindex-2a5edb96.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
