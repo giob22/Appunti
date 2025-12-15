dc65af4f4db5683d1342f55a358c44294500e668
giob22 <giovanni.fragola.nl@gmail.com>
2025-12-15 21:49:46 +0100

    aggiornamento automatico per appunti

--- synch_management/searcher-c2a407aa.js
+++ synch_management/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-52846d49.js',
+                path_to_root + 'searchindex-ba7dfa57.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
