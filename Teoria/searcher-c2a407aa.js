bb8e1e5597cb1ed0ed73ab9da86b216b1afda102
giob22 <giovanni.fragola.nl@gmail.com>
2025-12-25 20:00:32 +0100

    aggiornamento automatico per appunti

--- Teoria/searcher-c2a407aa.js
+++ Teoria/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-23f5c794.js',
+                path_to_root + 'searchindex-3688e5af.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
